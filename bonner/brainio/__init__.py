9e4753a43e7b20360fce3a88b831f6f03a069d48
Raj Magesh Gauthaman <raj.magesh@protonmail.com>
2022-07-20 18:49:51 -0400

    fix: minor

--- bonner/brainio/__init__.py
+++ bonner/brainio/__init__.py
@@ -1,4 +1,3 @@
-from .utils import BONNER_BRAINIO_HOME
 from .catalog import _import as import_catalog, _create as create_catalog
 from .network import fetch
 from .stimulus_set import package as package_stimulus_set
